6b4946acaf7963e8bd7e3930ebf2ab62c28d6f1a
uniquecodingtree@gmail.com <gzzhengshucong@GIH-D-8957.game.ntes>
2015-10-20 17:10:21 +0800

    将遮罩层相关样式整合进插件，并设置body不可滚动

--- src/jquery.popups.js
+++ src/jquery.popups.js
@@ -14,30 +14,51 @@
     openPopups: function () {
       //遮罩层
       $(this).before('<div id=\"zhezhao\"></div>');
+      //设置遮罩层样式
+      $('#zhezhao').css({
+        'width': '100%',
+        'height': '100%',
+        '_width': 'expression(eval(document.documentElement.clientWidth))',
+        '_height': 'expression(eval(document.documentElement.clientHeight))',
+        'position': 'fixed',
+        'left': '0',
+        'top': '0',
+        '_position':'absolute',
+        '_bottom':'auto',
+        '_top':'expression(eval(document.documentElement.scrollTop))',
+        'z-index': '500',
+        'background-color': '#333',
+        'filter': 'alpha(opacity=40)',
+        '-moz-opacity':'0.5',
+        '-khtml-opacity': '0.5',
+        'opacity': '0.4'
+      });
 
-      var windowHeight = $(window).height(),  //获取窗口的高度
-          windowWidth = $(window).width(),    //获取窗口的宽度
+      var windowHeight = $(window).height()-100,  //获取窗口的高度
+          windowWidth = $(window).width()-215,    //获取窗口的宽度
           popWidth = $(this).width(),         //获取弹窗的宽度
           popHeight = $(this).height(),       //获取弹窗高度
           popY,
           popX;
-
+      console.log(windowHeight+'-' +windowWidth+'-' +popHeight+'-' +popWidth);
       //计算弹出窗口的偏移量
       popY = (windowHeight - popHeight) / 2;
       popX = (windowWidth - popWidth) / 2;
 
       //设定窗口的居中弹出显示
       $(this).css({
-        position: 'fixed',
-        zIndex: '9999',
-        top: popY,
-        left: popX
+        'position': 'fixed',
+        'z-index': '9999',
+        'top': popY,
+        'left': popX
       }).show();
+      $('body').css('overflow', 'hidden');
     },
     closePopups: function (){
 
       $(this).hide();
       $(this).siblings('#zhezhao').hide();
+      $('body').css('overflow-y', 'auto');
     }
   }
 
